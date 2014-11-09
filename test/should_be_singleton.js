0776be20978a461000db19e33fe6674625dde6d2
Jonathan Birkholz <rookieone@gmail.com>
2014-11-08 22:51:06 -0600

    should be singleton

---
+++ test/should_be_singleton.js
@@ -0,0 +1,12 @@
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+var expect = require('chai').expect;
+
+lab.experiment('should return singleton', function() {
+  lab.test('same object', function(done) {
+    var c1 = require('../lib');
+    var c2 = require('../lib');
+    expect(c1).to.equal(c2);
+    done();
+  });
+});
