f1595c492b75cc1a34d4797949dd31b15784a694
Jonathan Birkholz <rookieone@gmail.com>
2014-12-10 14:55:59 -0600

    default logger allowing removal of console log

--- test/actions/error_message_if_cant_find_action.js
+++ test/actions/error_message_if_cant_find_action.js
@@ -4,14 +4,20 @@ var expect = require('chai').expect;
 
 lab.experiment('error message if cant find action', function() {
   var Commissar = {};
-
+  var lastLog = {};
   lab.before(function(done) {
     Commissar = require('../../lib').createInstance();
+    Commissar.logger = {
+      log: function() {
+        lastLog = arguments;
+      }
+    };
     done();
   });
 
   lab.test('should have error message', function(done) {
     Commissar.execute('some-unknown-action');
+    expect(lastLog[1].name).to.equal('some-unknown-action');
     done();
   });
 });
